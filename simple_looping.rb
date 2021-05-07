ce83146f60d4d1471a70cbbb9c1043d55a59c998
Evan Jeffries <ejeffries14@gmail.com>
2021-05-07 01:23:10 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -26,6 +26,7 @@ def while_iterator(number_of_times)
   phrase = "Welcome to Flatiron School's Web Development Course!"
   while number_of_times < number_of_times
   puts phrase
+  number_of_times += 1
   end
 end
 
